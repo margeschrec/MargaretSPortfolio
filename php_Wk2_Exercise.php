25ee5a1dd4c838549c9392ecc199f5b994267822
margeschrec <margeschrec@gmail.com>
2021-02-10 22:04:19 -0500

    work please? try again 16

---
+++ php_Wk2_Exercise.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+
+        </style>
+    </head>
+    <body>
+        <h1>I want some lunch</h1>
+        <form method="---php">
+            <input type="checkbox" id="food" name="food1" value="pasta"/>
+            <label for="food1"> pasta</label></br>
+            <input type="checkbox" id="food" name="food2" value="soup"/>
+            <label for="food2"> soup</label></br>
+            <input type="checkbox" id="food" name="food3" value="cheese"/>
+            <label for="food3"> cheese</label></form></br>
+            <input type="submit" value="Submit">
+        </form>
+        <?php
+        echo "<h1> anything </h1>";
+        ?>
+        
+    </body>
+</html>
