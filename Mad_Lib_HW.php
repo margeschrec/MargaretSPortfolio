51c52d4d3a061b5845393ebe440ba6702817fd1e
margeschrec <margeschrec@gmail.com>
2021-02-11 17:06:51 -0500

    Homework added week 2

--- Mad_Lib_HW.php
+++ Mad_Lib_HW.php
@@ -1,6 +1,32 @@
 <!doctype html>
 <html>
-    <head>
+
+<head>
 </head>
+
 <body>
-    <form action="---.php" meth
+    <h1> Mad Lib Week 2 </h1>
+    <div id="form">
+        <form action="Mad_Lib_HW.php" method="$_POST" name='madlib'>
+            color <input type="text" name="color"></br>
+            name <input type="text" name="name"></br>
+            place <input type="text" name="place"></br>
+            food <input type="text" name="food"></br>
+            <input type="submit" name="submit-btn" value="Go" />
+        </form>
+    </div>
+    </br>
+    <?php
+    if (isset($_POST['submit-btn'])) {
+        $color = $_POST["color"];
+        $name = $_POST["name"];
+        $place = $_POST["place"];
+        $food = $_POST["food"];
+        echo ("My $color cat is named $name");
+        echo ("They are from $place");
+        echo ("You can tell, because they eat $food");
+    }
+    ?>
+</body>
+
+</html>
