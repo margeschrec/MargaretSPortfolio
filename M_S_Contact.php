5fc2ad6b7b0e117c0ea69b42a813a4454ce95140
margeschrec <margeschrec@gmail.com>
2021-02-16 19:02:50 -0500

    updated CV and contact

--- M_S_Contact.php
+++ M_S_Contact.php
@@ -14,11 +14,20 @@
             <li><a href="M_S_Contact.php" class="navlinks">Contact</a></li>
         </ul>
     </nav>
-        <div name="form">
-            <form action="Margaret_Schrecongost.php" method="POST" name="nameform">
-                Name: <input type="text" name="name" placeholder="Type_Name_Here">
-                Email: <input type="text" name="email" placeholder="Email_Address">
-                <input type="submit" name="submit-btn" value="Go">
+        <div class="contactform">
+            <form action="Margaret_Schrecongost.php" method="POST" name="contactform">
+                <div class="cf-group">
+                    <label>Name:</label><input type="text" name="name" placeholder="Type_Name_Here">
+                </div>
+                <div class="cf-group">
+                 <label>Email:</label><input type="text" name="email" placeholder="Email_Address">
+                </div>
+                <div class="cf-group">
+                    <label for="Comments"> Comments, I wanna hear from you :)</label></br>
+                    <textarea id="Comments" name="Comments" rows="5" cols="70"></textarea>
+                </div>
+                </br>
+                <input class="button" type="submit" value="Submit">
             </form>
             <?php 
         if (isset($_Post['submit-btn'])) {
@@ -29,14 +38,6 @@
         ?>
         </div>
        
-        <div name=textimput>
-            <form method="Margaret_Schrecongost.php">
-                <label for="Comments"> Comments, I wanna hear from you :)</label></br>
-                <textarea id="Comments" name="Comments" rows="5" cols="70">
-                    Your reply here.
-                </textarea>
-                </br>
-                <input type="submit" value="Submit">
             </form>
         </div>
     </Body>
